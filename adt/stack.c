4444f5f098890f1bd06635f1fe4233b57d530926
bastianhs <140167233+bastianhs@users.noreply.github.com>
2023-11-20 13:37:16 +0700

    fix: modify and delete some function in stack

--- adt/stack.c
+++ adt/stack.c
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 
 /* ************ Prototype ************ */
+
 /* *** Konstruktor/Kreator *** */
+
 void CreateEmpty(Stack* S, long capacity) {
   /* I.S. sembarang; */
   /* F.S. Membuat sebuah stack S yang kosong berkapasitas capacity */
@@ -15,7 +17,7 @@ void CreateEmpty(Stack* S, long capacity) {
   Capacity(*S) = capacity;
 }
 
-void dealocateList(Stack* s) {
+void dealocateStack(Stack* s) {
   /* I.S. s terdefinisi; */
   /* F.S. (s) dikembalikan ke system, Top(s)=Nil; Capacity(s)=0 */
   free(Table(*s));
@@ -23,17 +25,20 @@ void dealocateList(Stack* s) {
   Capacity(*s) = 0;
 }
 
-/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+/* ************ Predikat untuk test keadaan KOLEKSI ************ */
+
 boolean IsEmpty(Stack S) {
   /* Mengirim true jika Stack kosong: lihat definisi di atas */
-  return Top(S) == Nil;
+  return Nil == Top(S);
 }
+
 boolean IsFull(Stack S) {
   /* Mengirim true jika tabel penampung nilai elemen stack penuh */
   return Top(S) == Capacity(S) - 1;
 }
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
+
 void Push(Stack* S, infotype X) {
   /* Menambahkan X sebagai elemen Stack S. */
   /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
@@ -48,9 +53,10 @@ void Push(Stack* S, infotype X) {
 }
 
 /* ************ Menghapus sebuah elemen Stack ************ */
+
 void Pop(Stack* S, infotype* X) {
   /* Menghapus X dari Stack S. */
-  /* I.S. S  tidak mungkin kosong */
+  /* I.S. S tidak mungkin kosong */
   /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
   *X = InfoTop(*S);
   if (Top(*S) == 0) {
@@ -60,7 +66,8 @@ void Pop(Stack* S, infotype* X) {
   }
 }
 
-/* ********* MENGUBAH UKURAN ARRAY ********* */
+/* ********* Mengubah Ukuran Stack ********* */
+
 void expandStack(Stack* s, long num) {
   /* Proses : Menambahkan capacity s sebanyak num */
   /* I.S. Stack sudah terdefinisi */
@@ -71,47 +78,11 @@ void expandStack(Stack* s, long num) {
 }
 
 void shrinkStack(Stack* s, long num) {
-  /* Proses : Mengurangi capacity sebanyak num */
+  /* Proses : Mengurangi capacity s sebanyak num */
   /* I.S. Stack sudah terdefinisi, ukuran capacity > num, dan nEff < capacity -
    * num. */
-  /* F.S. Ukuran list berkurang sebanyak num. */
+  /* F.S. Ukuran stack berkurang sebanyak num. */
   Table(*s) =
       (infotype*)realloc(Table(*s), (Capacity(*s) - num) * sizeof(infotype));
   Capacity(*s) -= num;
 }
-
-/* FITUR-FITUR UTAMA */
-void createNewDraft(Stack* stackDraft) {
-  printf("Masukkan draf:\n");
-  Word text;
-  int i = 0;
-  START();
-  while (!EOP && i < NMax) {
-    text.TabWord[i] = currentChar;
-    i++;
-    ADV();
-  }
-  text.Length = i;
-
-  if (isOnlySpace(text)) {
-    // Jika masukan tidak valid, menampilkan pesan bahwa draft gagal dibuat
-    printf("Draft tidak boleh hanya berisi spasi!\n");
-  } else {
-    print(
-        "Apakah anda ingin menghapus, menyimpan, atau menerbitkan draf ini?\n");
-    Word choice;
-    int i = 0;
-    START();
-    while (!EOP && i < NMax) {
-      choice.TabWord[i] = currentChar;
-      i++;
-      ADV();
-    }
-    choice.Length = i;
-  }
-}
-void saveDraft(Stack* stackDraft, Draft draft) {}
-void uploadDraft(ListDinTweet* listTweet, Draft draft) {}
-void displayLatestDraft(Stack stackDraft) {}
-void editLatestDraft(Stack* stackDraft) {}
-void deleteLatestDraft(Stack* stackDraft) {}
--- adt/stack.h
+++ adt/stack.h
@@ -20,9 +20,9 @@ typedef long address; /* indeks tabel */
 /* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
 typedef struct {
   infotype* T; /* tabel penyimpan elemen */
-  address TOP;
-  long capacity /* alamat TOP: elemen puncak */
-} Stack;
+  address TOP; /* alamat TOP: elemen puncak */
+  long capacity;
+} StackDinDraft;
 /* Definisi stack S kosong : S.TOP = Nil */
 /* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
 /* Jika S adalah Stack maka akses elemen : */
@@ -30,62 +30,59 @@ typedef struct {
 /* S.TOP adalah alamat elemen TOP */
 
 /* Definisi akses dengan Selektor : Set dan Get */
+
 #define Top(S) (S).TOP
 #define InfoTop(S) (S).T[(S).TOP]
 #define Table(S) (S).T
 #define Capacity(S) (S).capacity
 
 /* ************ Prototype ************ */
+
 /* *** Konstruktor/Kreator *** */
+
 void CreateEmpty(Stack* S, long capacity);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas capacity */
 /* jadi indeksnya antara 0.. capacity */
 /* Ciri stack kosong : TOP bernilai Nil */
 
-/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+void dealocateStack(Stack* s);
+/* I.S. s terdefinisi; */
+/* F.S. (s) dikembalikan ke system, Top(s)=Nil; Capacity(s)=0 */
+
+/* ************ Predikat untuk test keadaan KOLEKSI ************ */
+
 boolean IsEmpty(Stack S);
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
+
 boolean IsFull(Stack S);
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
+
 void Push(Stack* S, infotype X);
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
+
 void Pop(Stack* S, infotype* X);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
 
-/* FITUR-FITUR UTAMA */
-void createNewDraft(Stack* stackDraft);
-void saveDraft(Stack* stackDraft, Draft draft);
-void uploadDraft(ListDinTweet* listTweet, Draft draft);
-void displayLatestDraft(Stack stackDraft);
-void editLatestDraft(Stack* stackDraft);
-void deleteLatestDraft(Stack* stackDraft);
+/* ********* Mengubah Ukuran Stack ********* */
 
-/* FITUR TAMBAHAN*/
-boolean isSameWord(Word word1, Word word2) {
-  if (word1.Length != word2.Length) {
-    return false;
-  } else {
-    boolean same = true;
-    int i = 0;
-    while (same && i < word1.Length) {
-      if (word1.TabWord[i] != word2.TabWord[i]) {
-        same = false;
-      } else {
-        i++;
-      }
-    }
+void expandStack(Stack* s, long num);
+/* Proses : Menambahkan capacity s sebanyak num */
+/* I.S. Stack sudah terdefinisi */
+/* F.S. Ukuran Stack bertambah sebanyak num */
 
-    return same;
-  }
-}
+void shrinkStack(Stack* s, long num);
+/* Proses : Mengurangi capacity s sebanyak num */
+/* I.S. Stack sudah terdefinisi, ukuran capacity > num, dan nEff < capacity -
+ * num. */
+/* F.S. Ukuran stack berkurang sebanyak num. */
 
 #endif
